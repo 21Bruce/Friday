4aac3d4873b0d81f6f0e81bea3ea1a3747a2fed7
Bruce Jagid <brucejagid@gmail.com>
2021-07-18 01:12:19 -0400

    Initial Commit

---
+++ Friday2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friday2
+//
+//  Created by Bruce Jagid on 7/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friday2/Friday2App.swift
@@ -0,0 +1,17 @@
+//
+//  Friday2App.swift
+//  Friday2
+//
+//  Created by Bruce Jagid on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Friday2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
