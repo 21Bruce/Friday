678f0c17cde6a8b1ec1865a45b34996cec055e62
Amurph733 <amurph733@gmail.com>
2021-07-22 11:19:54 -0400

    almost done with feature

--- Friday2/Views/OrganizerView.swift
+++ Friday2/Views/OrganizerView.swift
@@ -8,76 +8,126 @@
 import SwiftUI
 
 struct OrganizerView: View {
-    @Binding var acceptedUsers: [UIImage]
-//    @Binding var pendingUsers: [UIImage]
-//    @Binding var rejectedUsers: [UIImage]
+    var acceptedUsers: [UIImage] =
+        [UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!,
+         UIImage(systemName: "heart.fill")!
+        ]
+    var pendingUsers: [UIImage] = [UIImage(systemName: "heart.fill")!, UIImage(systemName: "heart.fill")!]
+    var rejectedUsers: [UIImage] = [UIImage(systemName: "heart.fill")!]
 //OLD CODE STARTED HERE, COPY ONTO LINE BELOW
     var body: some View {
         ZStack{
             OrganizerViewBackground()
-            OrganizerViewText(acceptedUsers: acceptedUsers)
+            OrganizerViewText(acceptedUsers: acceptedUsers, pendingUsers: pendingUsers, rejectedUsers: rejectedUsers)
         }
-        .frame(width: 350, height: 500, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .frame(width: 350, height: 500)
     }
     
 
 }
 
 struct OrganizerViewBackground: View{
+    var cornerRadius: CGFloat = 25.0
+    var rectangleColor: Color = Color(red: 40/255 , green:221/255, blue:249/255)
+    var borderColor: Color = Color(.black)
+
     var body: some View{
         ZStack{
-            RoundedRectangle(cornerRadius: 25.0)
-            .foregroundColor(Color(red: 40/255 , green:221/255, blue:249/255))
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerRadius: cornerRadius)
+            .foregroundColor(rectangleColor)
+            RoundedRectangle(cornerRadius: cornerRadius)
                 .stroke()
-                .foregroundColor(.black)
+                .foregroundColor(borderColor)
         }
     }
 }
 
 struct OrganizerViewText: View {
     var acceptedUsers: [UIImage]
-    //@Binding var pendingUsers: [UIImage]
-    //@Binding var rejectedUsers: [UIImage]
-    
+    var pendingUsers: [UIImage]
+    var rejectedUsers: [UIImage]
+    var profileSpacing: CGFloat = 10
+    var profilePicSize: CGFloat = 50
+    var textSize: CGFloat = 15
+
     var body: some View{
-        VStack{
+        VStack(spacing:0){
             HStack{
                 Text("Accepted")
+                    .frame(maxWidth: .infinity)
                 Text("Pending")
+                    .frame(maxWidth: .infinity)
                 Text("Rejected")
+                    .frame(maxWidth: .infinity)
+            }
+            .frame(maxWidth: .infinity)
+            .font(.custom("Avenir Medium", size:textSize))
+            .padding()
             NiceDivider()
-                ScrollView{
-                    HStack{
-                        VStack{
-                            ForEach(acceptedUsers, id: \.self) {UIImage in
-                                ProfileIcon(uiImage: UIImage)
-                            }
-                        }
-                        VStack{
-                            
-                        }
-                        VStack{
-                       
+            ScrollView{
+                HStack{
+                    VStack(spacing: profileSpacing){
+                        ForEach(acceptedUsers, id: \.self) {UIImage in
+                            ProfileIcon(uiImage: UIImage)
+                                .frame(width: profilePicSize, height: profilePicSize)
                         }
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    VStack(spacing: profileSpacing){
+                        ForEach(pendingUsers, id: \.self) {UIImage in
+                            ProfileIcon(uiImage: UIImage)
+                                .frame(width: profilePicSize, height: profilePicSize)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    VStack(spacing: profileSpacing){
+                        ForEach(rejectedUsers, id: \.self) {UIImage in
+                            ProfileIcon(uiImage: UIImage)
+                                .frame(width: profilePicSize, height: profilePicSize)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                 }
+                .padding(.bottom, profileSpacing)
+                .padding(.top, profileSpacing)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
+            .padding(.leading)
+            .padding(.trailing)
         }
     }
 }
+
+struct OrganizerView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrganizerView()
+    }
+}
+
+
+
+
+
 
 
 #if DEBUG
 
 struct OrganizerViewContainerPreview: View {
     @State var acceptedUsers: [UIImage]
-    
+
     var body: some View {
         OrganizerView(acceptedUsers: $acceptedUsers)
     }
 }
-    
+
 struct OrganizerView_Previews: PreviewProvider {
     static var previews: some View {
         OrganizerView(acceptedUsers: [
