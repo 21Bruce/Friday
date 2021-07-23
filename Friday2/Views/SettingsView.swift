101c7c600d1f3bcb18ae324e9fe0ab8c28673721
Amurph733 <amurph733@gmail.com>
2021-07-23 11:41:53 -0400

    updated settings with CNiceDividers and organized

--- Friday2/Views/SettingsView.swift
+++ Friday2/Views/SettingsView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct SettingsView: View {
+    var SettingsTextPadding: CGFloat = 6
+    var SettingsTextSize: CGFloat = 20
+    var DividerColor: Color = Color(red: 200/255, green: 200/255, blue: 200/255)
+    var DividerWidth: CGFloat = 1
+    var SettingsColor: Color = Color(.black)
+    
     var body: some View {
-        VStack(alignment: .trailing){
+        VStack(alignment: .trailing, spacing: 0){
             ZStack{
                 HStack{
                     HStack{
-                    Image(systemName: "gear")
-                    Text("Settings")
-                        .font(.custom("Avenir Medium", size: 20))
+                        Image(systemName: "gear")
+                        Text("Settings")
+                            .font(.custom("Avenir Medium", size: SettingsTextSize))
                     }
+                    .foregroundColor(SettingsColor)
                     .padding(.horizontal)
                     Spacer()
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
@@ -26,134 +33,140 @@ struct SettingsView: View {
                     })
                     .buttonStyle(BorderlessButtonStyle())
                 }
-                    }
-            ScrollView() {
-                    VStack(alignment: .leading, spacing:0){
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack{
-                                Rectangle()
-                                .stroke()
-                                    .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255)
-                            )
-                                    .frame(maxWidth:.infinity)
-                                HStack{
-                            Image(systemName: "camera.circle.fill")
-                                .foregroundColor(.black)
-                            Text("Change Profile Picture")
-                                .font(.custom("Avenir Medium", size: 18)) 
-                                .foregroundColor(.black)
-                                    Image(systemName: "arrow.right")
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                        }
-                        })
-                .buttonStyle(BorderlessButtonStyle())
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack{
-                                Rectangle()
-                                .stroke()
-                                    .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255)
-                            )
-                                    .frame(maxWidth:.infinity)
-                                HStack{
-                            Image(systemName: "bell")
-                                .foregroundColor(.black)
-                            Text("Notifications")
-                                .font(.custom("Avenir Medium", size: 18))
-                                .foregroundColor(.black)
-                                    Image(systemName: "arrow.right")
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                        }
-                        })
-                .buttonStyle(BorderlessButtonStyle())
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack
-                            {
-                                Rectangle()
-                                .stroke()
-                                    .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255)
-                            )
-                                    .frame(maxWidth:.infinity)
-                                HStack{
-                            Image(systemName: "person.badge.minus")
-                                .foregroundColor(.black)
-                            Text("Sign Out")
-                                .font(.custom("Avenir Medium", size: 18))
-                                .foregroundColor(.black)
-                                    Image(systemName: "arrow.right")
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                        }
-                        })
-                        .buttonStyle(BorderlessButtonStyle())
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack
-                            {
-                                Rectangle()
-                                .stroke()
-                                    .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255)
-                            )
-                                    .frame(maxWidth:.infinity)
-                                HStack{
-                            Image(systemName:"rectangle.fill.on.rectangle.fill")
-                                .foregroundColor(.black)
-                            Text("Change Background")
-                                .font(.custom("Avenir Medium", size: 18))
-                                .foregroundColor(.black)
-                                    Image(systemName: "arrow.right")
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                        }
-                        })
-                        .buttonStyle(BorderlessButtonStyle())
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack
-                            {
-                                Rectangle()
-                                .stroke()
-                                    .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255)
-                            )
-                                    .frame(maxWidth:.infinity)
-                                HStack{
-                            Image(systemName:"questionmark.circle.fill")
-                                .foregroundColor(.black)
-                            Text("Help")
-                                .font(.custom("Avenir Medium", size: 18))
-                                .foregroundColor(.black)
-                                    Image(systemName: "arrow.right")
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                        }
-                        })
-                        .buttonStyle(BorderlessButtonStyle())
-                        
             }
+            .padding(.bottom, SettingsTextPadding)
+            .padding(.top, SettingsTextPadding)
+            CNiceDividers(Color: DividerColor, Width: DividerWidth)
+            ScrollView() {
+                VStack(alignment: .leading, spacing:0){
+                    CNiceDividers(Color: DividerColor, Width: DividerWidth)
+                    Button(action: {}, label: {
+                        SettingTab(
+                            IconImage: "camera.circle.fill",
+                            SettingName: "Change Profile Picture"
+                        )
+                    })
+                    .buttonStyle(BorderlessButtonStyle())
+                    Button(action: {}, label: {
+                        SettingTab(
+                            IconImage: "bell",
+                            SettingName: "Notifications"
+                        )
+                    })
+                    .buttonStyle(BorderlessButtonStyle())
+                    Button(action: {}, label: {
+                        SettingTab(
+                            IconImage: "person.badge.minus",
+                            SettingName: "Sign Out"
+                        )
+                    })
+                    .buttonStyle(BorderlessButtonStyle())
+                    Button(action: {}, label: {
+                        SettingTab(
+                            IconImage: "rectangle.fill.on.rectangle.fill",
+                            SettingName: "Change Background"
+                        )
+                    })
+                    .buttonStyle(BorderlessButtonStyle())
+                    Button(action: {}, label: {
+                        SettingTab(
+                            IconImage: "questionmark.circle.fill",
+                            SettingName: "Help"
+                        )
+                    })
+                    .buttonStyle(BorderlessButtonStyle())
+                }
+            }
+
         }
     }
 }
+
+struct SettingTab: View {
+    var DividerColor: Color = Color(red: 200/255, green: 200/255, blue: 200/255)
+    var SettingFontSize: CGFloat = 18
+    var SettingColor: Color = Color(.black)
+    var PaddingSpace: CGFloat = 9
+    var IconImage: String
+    var SettingName: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack{
+                Image(systemName: IconImage)
+                Text(SettingName)
+                    .font(.custom("Avenir Medium", size: SettingFontSize))
+                Image(systemName: "arrow.right")
+                Spacer()
+            }
+            .foregroundColor(SettingColor)
+            .padding(.leading, PaddingSpace * 1.5)
+            .padding(.bottom, PaddingSpace)
+            CNiceDividers(Color: DividerColor, Width: 1)
+        }
+        .padding(.top, PaddingSpace)
+    }
 }
 
-    
-    
-    
-    
-    
-    
-    
-    
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
     }
 }
+
+//MARK: Implementation Code
+
+struct ModifiedSettingTab: View {
+    var DividerColor: Color = Color(red: 200/255, green: 200/255, blue: 200/255)
+    var SettingFontSize: CGFloat = 18
+    var SettingColor: Color = Color(.black)
+    var PaddingSpace: CGFloat = 9
+    var IconImage: String
+    var SettingName: String
+    var Place: String
+    
+    
+    var body: some View {
+        Button(action: {
+            //Navigation link to place under var Place
+        },
+        label: {
+            VStack(spacing: 0) {
+                HStack{
+                    Image(systemName: IconImage)
+                    Text(SettingName)
+                        .font(.custom("Avenir Medium", size: SettingFontSize))
+                    Image(systemName: "arrow.right")
+                    Spacer()
+                }
+                .foregroundColor(SettingColor)
+                .padding(.leading, PaddingSpace * 1.5)
+                .padding(.bottom, PaddingSpace)
+                CNiceDividers(Color: DividerColor, Width: 1)
+            }
+            .padding(.top, PaddingSpace)
+        })
+    }
+}
+
+//MARK: OLD CODE
+
+        //        ZStack{
+        //            Rectangle()
+        //                .stroke()
+        //                .foregroundColor(DividerColor)
+        //                .frame(maxWidth:.infinity)
+        //            HStack{
+        //                Image(systemName: IconImage)
+        //                    .foregroundColor(.black)
+        //                Text(SettingName)
+        //                    .font(.custom("Avenir Medium", size: SettingFontSize))
+        //                    .foregroundColor(SettingColor)
+        //                Image(systemName: "arrow.right")
+        //                    .foregroundColor(SettingColor)
+        //                Spacer()
+        //            }
+        //            .padding(10)
+        //        }
+            
+        
